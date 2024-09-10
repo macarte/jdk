d145b7a4f70819954502d55ae173d54ea95892ba
Matthew Carter <macarte@microsoft.com>
2024-09-09 17:01:16 -0700

    test method flag for c1 and c2

--- src/hotspot/share/runtime/sharedRuntime.cpp
+++ src/hotspot/share/runtime/sharedRuntime.cpp
@@ -1327,32 +1327,34 @@ methodHandle SharedRuntime::find_callee_method(TRAPS) {
 
 void SharedRuntime::trigger_action_from_c1(JavaThread* current)
 {
-  vframeStream vfst(current);//, true);
-  if (!vfst.at_end()) {
+  //vframeStream vfst(current);//, true);
+  //if (!vfst.at_end()) {
     //assert(!vfst.at_end(), "Java frame must exist");
     ResourceMark rm(current);
-    methodHandle caller(current, vfst.method());
-    if(caller->is_trigger()) {
-      JRT_BLOCK
-        SharedRuntime::trigger_action("from c1", CHECK);
-      JRT_BLOCK_END
+    Method* method = current->callee_target();
+    //methodHandle caller(current, vfst.method());
+    if(method->is_trigger()) {
+      //JRT_BLOCK
+        SharedRuntime::trigger_action("from c1", current);//CHECK);
+      //JRT_BLOCK_END
     }
-  }
+  //}
 }
 
-JRT_LEAF(void, SharedRuntime::trigger_action_from_c2(JavaThread* current))
+JRT_BLOCK_ENTRY(void, SharedRuntime::trigger_action_from_c2(JavaThread* current))
 {
-  vframeStream vfst(current);//, true);
-  if (!vfst.at_end()) {
+  //vframeStream vfst(current);//, true);
+  //if (!vfst.at_end()) {
     //assert(!vfst.at_end(), "Java frame must exist");
     ResourceMark rm(current);
-    methodHandle caller(current, vfst.method());
-    if(caller->is_trigger()) {
+    Method* method = current->callee_target();
+   // methodHandle method = callee->is_tr
+    if(method->is_trigger()) {
       JRT_BLOCK
         SharedRuntime::trigger_action("from c2", CHECK);
       JRT_BLOCK_END
     }
-  }
+  //}
 }
 JRT_END
 
